0a49951f86fd15cfdee9ef9678cf862f2d2c4d3c
BM Devs <bouletmarc@hotmail.com>
2017-04-08 08:49:16 -0400

    V1.2.4

--- BMDatalogger/LoadSave.ino
+++ BMDatalogger/LoadSave.ino
@@ -1,39 +1,36 @@
 void SaveOptions() {
-  for (int i=0; i < 9; i++) {
-    int Addr = i * 10;
-    if (i == 0) EEPROM.put(Addr, Timeout);
-    if (i == 1) EEPROM.put(Addr, Injectors_Size);
-    if (i == 2) EEPROM.put(Addr, mBarSeaLevel);
-    if (i == 3) EEPROM.put(Addr, TrannyType);
-    if (i == 4) EEPROM.put(Addr, O2Input);
-    if (i == 5) EEPROM.put(Addr, MapValue);
-    if (i == 6) EEPROM.put(Addr, UseCelcius);
-    if (i == 7) EEPROM.put(Addr, UseKMH);
-    if (i == 8) EEPROM.put(Addr, ScreenIndex);
-    if (i == 9) EEPROM.put(Addr, UseLAMBA);
-    if (i == 10) EEPROM.put(Addr, WBType);
+  EEPROM.put(0, Timeout);
+  EEPROM.put(5, Injectors_Size);
+  EEPROM.put(10, mBarSeaLevel);
+  EEPROM.put(15, TrannyType);
+  EEPROM.put(16, O2Input);
+  EEPROM.put(17, MapValue);
+  EEPROM.put(18, UseCelcius);
+  EEPROM.put(19, UseKMH);
+  EEPROM.put(20, UseLAMBA);
+  EEPROM.put(21, WBType);
+  
+  EEPROM.put(100, ScreenIndex);
 
-    //Write 3 at the last location to tell data ever get saved
-    EEPROM.write(1023, 3);
-  }
+  //Write 4 at the last location to tell data ever get saved
+  EEPROM.write(1023, 4);
 }
 
 void LoadOptions() {
-  if (EEPROM.read(1023) == 3) {
-    for (int i=0; i < 9; i++) {
-      int Addr = i * 10;
-      if (i == 0) EEPROM.get(Addr, Timeout);
-      if (i == 1) EEPROM.get(Addr, Injectors_Size);
-      if (i == 2) EEPROM.get(Addr, mBarSeaLevel);
-      if (i == 3) EEPROM.get(Addr, TrannyType);
-      if (i == 4) EEPROM.get(Addr, O2Input);
-      if (i == 5) EEPROM.get(Addr, MapValue);
-      if (i == 6) EEPROM.get(Addr, UseCelcius);
-      if (i == 7) EEPROM.get(Addr, UseKMH);
-      if (i == 8) EEPROM.get(Addr, ScreenIndex);
-      if (i == 9) EEPROM.get(Addr, UseLAMBA);
-      if (i == 10) EEPROM.get(Addr, WBType);
-    }
+  if (EEPROM.read(1023) == 4) {
+    EEPROM.get(0, Timeout);
+    EEPROM.get(5, Injectors_Size);
+    EEPROM.get(10, mBarSeaLevel);
+    EEPROM.get(15, TrannyType);
+    EEPROM.get(16, O2Input);
+    EEPROM.get(17, MapValue);
+    EEPROM.get(18, UseCelcius);
+    EEPROM.get(19, UseKMH);
+    EEPROM.get(20, UseLAMBA);
+    EEPROM.get(21, WBType);
+
+    
+    EEPROM.get(100, ScreenIndex);
   }
   else
     SaveOptions();
