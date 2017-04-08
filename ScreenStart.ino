5344d2a7043c94a689171407271b501e2a99946a
BM Devs <bouletmarc@hotmail.com>
2017-04-08 08:09:59 -0400

    Add files via upload

---
+++ ScreenStart.ino
@@ -0,0 +1,19 @@
+void StartScreen(){
+  String T1 = " BMDatalogger" + VersionStr;
+  String T2 = "   eCtune ISR V3    ";
+  String T3 = "  Datalog Protocol  ";
+  String T4 = "  Made by BM Devs   ";
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(T1);
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print(T2);
+  delay(1000);
+  lcd.setCursor(0,2);
+  lcd.print(T3);
+  lcd.setCursor(0,3);
+  lcd.print(T4);
+  delay(2000);  
+}
