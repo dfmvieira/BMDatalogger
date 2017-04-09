7edb5385b2f032e4201af39260e537b7a92b5aed
BM Devs <bouletmarc@hotmail.com>
2017-04-09 11:13:04 -0400

    V1.3.3

--- BMDatalogger/ProgressBar.ino
+++ BMDatalogger/ProgressBar.ino
@@ -10,7 +10,7 @@ byte _level0[8] = {
     B11111
 };
 // -- character with one bars
-/*byte _level1[8] = {
+byte _level1[8] = {
     B10000,
     B10000,
     B10000,
@@ -52,7 +52,7 @@ byte _level4[8] = {
     B11110,
     B11110,
     B11110
-};*/
+};
 
 //###########################################################################################################
 //###########################################################################################################
@@ -60,10 +60,10 @@ byte _level4[8] = {
 
 void GraphInit() {
   lcd.createChar(10, _level0);
-  /*lcd.createChar(11, _level1);
+  lcd.createChar(11, _level1);
   lcd.createChar(12, _level2);
   lcd.createChar(13, _level3);
-  lcd.createChar(14, _level4);*/
+  lcd.createChar(14, _level4);
   
 }
 
@@ -76,13 +76,49 @@ void GraphSetValue(int minValue, int maxValue) {
 }
 
 void GraphDrawValue(int value, int Lenght) {
-  int Count = GraphMinValue + value * Lenght / GraphMaxValue;
+  int percent = (GraphMinValue + value) * GraphMaxValue / 100;
+  int a = Lenght/100*percent;
+  char b = 0;
+  
+  // drawing black rectangles on LCD
+  if (a>=1) {
+    for (int i=1;i<a;i++) {
+      lcd.write((uint8_t)10);
+      b=i;
+    }
+    a=a-b;
+  }
+  
+  unsigned int peace = a*5;
+  
+  // drawing charater's colums
+  switch (peace) {
+    case 0:
+      break;
+    case 1:
+      lcd.write((uint8_t)10 + 1);
+      break;
+    case 2:
+      lcd.write((uint8_t)10 + 2);
+      break;
+    case 3:
+      lcd.write((uint8_t)10 + 3);
+      break;
+    case 4:
+      lcd.write((uint8_t)10 + 4);
+      break;
+  }
+  
+  //clearing line
+  for (int i =0;i<(Lenght-b);i++) lcd.print(" ");
+  
+  /*int Count = GraphMinValue + value * Lenght / GraphMaxValue;
   int CountHalf = (GraphMinValue + value * Lenght * 5 / GraphMaxValue) % 5;
 
   for(byte i=0; i < Count; i++) lcd.write((uint8_t)10);
-  /*if(CountHalf > 0) {
+  if(CountHalf > 0) {
     lcd.write((char) CountHalf + 10);
     Count++;
-  }*/
-  for(byte i=0; i < Lenght - Count; i++) lcd.write(' ');
+  }
+  for(byte i=0; i < Lenght - Count; i++) lcd.write(' ');*/
 }
--- BMDatalogger/ScreenStart.ino
+++ BMDatalogger/ScreenStart.ino
@@ -1,63 +1,29 @@
 void StartScreen(){
-  String T1 = "BMDatalogger " + VersionStr + " ";
+  /*String T1 = "BMDatalogger " + VersionStr + " ";
   String T2 = "   eCtune ISR V3    ";
   String T3 = "  Datalog Protocol  ";
   String T4 = "  Made by BM Devs   ";
 
   lcd.clear();
   //render_big_msg("HONDA",0,0);
-  /*writeBigString("HONDA", 0, 0);
+  writeBigString("HONDA", 0, 0);
   lcd.setCursor(0,2);
   lcd.print(T1);
   lcd.setCursor(0,3);
   lcd.print(T2);
   delay(2000);*/
-  
+
+  lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print(T1);
+  lcd.print("BMDatalogger " + VersionStr + " ");
   delay(1000);
   lcd.setCursor(0,1);
-  lcd.print(T2);
+  lcd.print("   eCtune ISR V3    ");
   delay(1000);
   lcd.setCursor(0,2);
-  lcd.print(T3);
+  lcd.print("  Datalog Protocol  ");
   delay(1000);
   lcd.setCursor(0,3);
-  lcd.print(T4);
-  //delay(2000);
-  delay(1000);
-  if (!GetJ12Cut()) {
-    T1 += "    NOT CONNECTED   ";
-    T2 += "   IS J12 INPLACE?  ";
-    T4 += "    PUSH TO START   "; 
-  }
-  else 
-  {
-    T1 += "     CONNECTED      ";
-    T2 += "                    ";
-    T4 += "    WAIT TO START   "; 
-  }
-  T3 += "                    ";
-  delay(1000);
-
-  //Display Scroll (20px)
-  for (int i=0; i<20; i++) {
-    T1.remove(0, 1);
-    T2.remove(0, 1);
-    T3.remove(0, 1);
-    T4.remove(0, 1);
-    T1 += " ";
-    T2 += " ";
-    T3 += " ";
-    T4 += " ";
-    lcd.setCursor(0,0);
-    lcd.print(T1);
-    lcd.setCursor(0,1);
-    lcd.print(T2);
-    lcd.setCursor(0,2);
-    lcd.print(T3);
-    lcd.setCursor(0,3);
-    lcd.print(T4);
-    delay(75);
-  }
+  lcd.print("  Made by BM Devs   ");
+  delay(2000);
 }
